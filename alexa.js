f0e639000cb56df38aeb0910eb4c1d25e4c67404
soef <soef@gmx.net>
2017-11-30 22:47:07 +0100

    working on...

--- alexa.js
+++ alexa.js
@@ -1,5 +1,4 @@
-﻿
-"use strict";
+﻿"use strict";
 
 const
     soef = require('soef'),
@@ -21,9 +20,10 @@ function onUnload(callback) {
 }
 
 function onStateChange(id, state) {
-    let ar, [a, inst, device, channel, subChannel] = ar = id.split('.');
+    let ar, [a, inst, deviceId, channel, subChannel] = ar = id.split('.');
 
     let func = devices.get(id.substr(8)).func;
+    let device = alexa.serialNumbers[deviceId];
 
     switch(channel) {
         case 'Bluetooth':
@@ -31,12 +31,10 @@ function onStateChange(id, state) {
             if (typeof func === 'function') func(ar[5] === 'connect');
             break;
         default:
-            if (typeof func === 'function') func(state.val);
+            if (typeof func === 'function') func(state.val, device, id);
             break;
     }
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // function onObjectChange(id, object) {
 // }
@@ -57,24 +55,43 @@ const
         repeat: { val: false, common: {role: 'button'}},
     };
 
+
+function setRequestResult(err, res) {
+    if (!err) return;
+    devices.root.setAndUpdate('requestResult', err);
+}
+
+Alexa.prototype.updateDevices = function (callback) {
+    Object.keys (this.serialNumbers).forEach ((n) => {
+        let st;
+        let device = this.serialNumbers[n];
+        let dev = new devices.CDevice (device.serialNumber, device._name);
+        dev.set ('', device.online ? 'Online' : 'offline');
+    });
+    devices.update(callback);
+};
+
 Alexa.prototype.createDevices = function (callback) {
 
     Object.keys (this.serialNumbers).forEach ((n) => {
+        let st;
         let device = this.serialNumbers[n];
         let dev = new devices.CDevice(device.serialNumber, device._name);
-        dev.setChannel('properties');
+        dev.set('', device.online ? 'Online' : 'offline');
+        dev.set('.requestResult', { val: '', common: { name: 'Request Result', write: false }});
+        dev.setChannel('states');
         dev.set('capabilities', device.capabilities.join(','));
         if (device.bluetoothState) {
             //dev.setChannel('Bluetooth');
             device.bluetoothState.pairedDeviceList.forEach((bt) => {
                 dev.setChannel('Bluetooth.' + bt.address, bt.friendlyName);
-                let st = dev.oset('connect', { val: false, common: { role: 'button'}});
+                st = dev.oset('connect', { val: false, common: { role: 'button'}});
                 if (st) st.func = bt.connect;
                 st = dev.oset('disconnect', { val: false, common: { role: 'button'}});
                 if (st) st.func = bt.connect;
             });
         }
-        dev.setChannel('ToDos');
+        //dev.setChannel('ToDos');
 
         dev.setChannel('Commands');
         for (let n in commands) {
@@ -83,12 +100,22 @@ Alexa.prototype.createDevices = function (callback) {
         }
 
         if (device.capabilities.includes('TUNE_IN')) {
-            dev.set ('TuneIn', "");
+            st = dev.oset ('TuneIn', "");
+            if (st) st.func = function (query, device, id) {
+                if (!device) return;
+                alexa.tuneinSearch(query, function(err, res) {
+                    setRequestResult(err, res);
+                     if (err || !res || !Array.isArray(res.browseList)) return;
+                     let station = res.browseList[0];
+                     device.setTunein(station.id, station.contentType);
+                     devices.root.setAndUpdate(id, { val: station.name, ack: true });
+                })
+
+            }
         }
     });
     devices.update();
 };
-
 
 
 function main() {
@@ -101,6 +128,17 @@ function main() {
             bluetooth: true
         },
         function () {
+
+            // alexa.getPlayerInfo('Wohnzimmer', function(err, res) {
+            //     res = res;
+            //     if (res.playerInfo.volume) {
+            //         let vol = res.playerInfo.volume.volume;
+            //         let muted = res.playerInfo.volume.muted;
+            //     }
+            //     let state = res.playerInfo.state; // PAUSED
+            // })
+
+
             alexa.createDevices();
 
             adapter.subscribeStates('*');
@@ -108,3 +146,8 @@ function main() {
         }
     )
 }
+
+
+//api/tts/audio/data
+
+
