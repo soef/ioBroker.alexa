bebc88a4d2d5fe9f395ca850f7bc0d5bcd0874a5
Ingo Fischer <github@fischer-ka.de>
2020-03-17 07:24:52 +0100

    3.1.0 * fixes #371 * fixes #360

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ const shObjects = require(path.join(__dirname, 'lib', 'smarthomedevices.js'));
 const bespokenVdSDK = require("virtual-device-sdk");
 let bespokenDevice;
 
-const Sentry = require('@sentry/node');
-const SentryIntegrations = require('@sentry/integrations');
-const packageJson = require('./package.json');
+let Sentry;
+let SentryIntegrations;
 
 const forbiddenCharacters = /[\]\[*,;'"`<>\\\s?]/g;
 
@@ -369,6 +368,102 @@ function processObjectQueue(callback) {
     });
 }
 
+function initSentry(callback) {
+    if (!adapter.ioPack.common || !adapter.ioPack.common.plugins || !adapter.ioPack.common.plugins.sentry) {
+        return callback && callback();
+    }
+    const sentryConfig = adapter.ioPack.common.plugins.sentry;
+    if (!sentryConfig.dsn) {
+        adapter.log.warn('Invalid Sentry definition, no dsn provided. Disable error reporting');
+        return callback && callback();
+    }
+    // Require needed tooling
+    Sentry = require('@sentry/node');
+    SentryIntegrations = require('@sentry/integrations');
+    // By installing source map support, we get the original source
+    // locations in error messages
+    require('source-map-support').install();
+
+    let sentryPathWhitelist = [];
+    if (sentryConfig.pathWhitelist && Array.isArray(sentryConfig.pathWhitelist)) {
+        sentryPathWhitelist = sentryConfig.pathWhitelist;
+    }
+    if (adapter.pack.name && !sentryPathWhitelist.includes(adapter.pack.name)) {
+        sentryPathWhitelist.push(adapter.pack.name);
+    }
+    let sentryErrorBlacklist = [];
+    if (sentryConfig.errorBlacklist && Array.isArray(sentryConfig.errorBlacklist)) {
+        sentryErrorBlacklist = sentryConfig.errorBlacklist;
+    }
+    if (!sentryErrorBlacklist.includes('SyntaxError')) {
+        sentryErrorBlacklist.push('SyntaxError');
+    }
+
+    Sentry.init({
+        release: adapter.pack.name + '@' + adapter.pack.version,
+        dsn: sentryConfig.dsn,
+        integrations: [
+            new SentryIntegrations.Dedupe()
+        ]
+    });
+    Sentry.configureScope(scope => {
+        scope.setTag('version', adapter.common.installedVersion || adapter.common.version);
+        if (adapter.common.installedFrom) {
+            scope.setTag('installedFrom', adapter.common.installedFrom);
+        }
+        else {
+            scope.setTag('installedFrom', adapter.common.installedVersion || adapter.common.version);
+        }
+        scope.addEventProcessor(function(event, hint) {
+            // Try to filter out some events
+            if (event && event.metadata) {
+                if (event.metadata.function && event.metadata.function.startsWith('Module.')) {
+                    return null;
+                }
+                if (event.metadata.type && sentryErrorBlacklist.includes(event.metadata.type)) {
+                    return null;
+                }
+                if (event.metadata.filename && !sentryPathWhitelist.find(path => path && path.length && event.metadata.filename.includes(path))) {
+                    return null;
+                }
+                if (event.exception && event.exception.values && event.exception.values[0] && event.exception.values[0].stacktrace && event.exception.values[0].stacktrace.frames) {
+                    for (let i = 0; i < (event.exception.values[0].stacktrace.frames.length > 5 ? 5 : event.exception.values[0].stacktrace.frames.length); i++) {
+                        let foundWhitelisted = false;
+                        if (event.exception.values[0].stacktrace.frames[i].filename && sentryPathWhitelist.find(path => path && path.length && event.exception.values[0].stacktrace.frames[i].filename.includes(path))) {
+                            foundWhitelisted = true;
+                            break;
+                        }
+                        if (!foundWhitelisted) {
+                            return null;
+                        }
+                    }
+                }
+            }
+
+            return event;
+        });
+
+        adapter.getForeignObject('system.config', (err, obj) => {
+            if (obj && obj.common && obj.common.diag) {
+                adapter.getForeignObject('system.meta.uuid', (err, obj) => {
+                    // create uuid
+                    if (!err  && obj) {
+                        Sentry.configureScope(scope => {
+                            scope.setUser({
+                                id: obj.native.uuid
+                            });
+                        });
+                    }
+                    callback && callback();
+                });
+            }
+            else {
+                callback && callback();
+            }
+        });
+    });
+}
+
 function startAdapter(options) {
     options = options || {};
     Object.assign(options, {
@@ -419,7 +514,6 @@ function startAdapter(options) {
             if (object && object.common && object.common.name) {
                 if (typeof device.rename === 'function') device.rename(object.common.name);
             }
-            return;
         }
     });
 
@@ -428,41 +522,12 @@ function startAdapter(options) {
     });
 
     adapter.on('ready', () => {
-        Sentry.init({
-            release: packageJson.name + '@' + packageJson.version,
-            dsn: 'https://dcc71ec0ea3244ebb0cf5ac0b3ccd120@sentry.io/1839681',
-            integrations: [
-                new SentryIntegrations.Dedupe()
-            ]
-        });
-        Sentry.configureScope(scope => {
-            scope.setTag('version', adapter.common.installedVersion || adapter.common.version);
-            if (adapter.common.installedFrom) {
-                scope.setTag('installedFrom', adapter.common.installedFrom);
-            }
-            else {
-                scope.setTag('installedFrom', adapter.common.installedVersion || adapter.common.version);
-            }
-        });
-
-        adapter.getForeignObject('system.config', (err, obj) => {
-            if (obj && obj.common && obj.common.diag) {
-                adapter.getForeignObject('system.meta.uuid', (err, obj) => {
-                    // create uuid
-                    if (!err  && obj) {
-                        Sentry.configureScope(scope => {
-                            scope.setUser({
-                                id: obj.native.uuid
-                            });
-                        });
-                    }
-                    loadExistingAccessories(main);
-                });
-            }
-            else {
-                loadExistingAccessories(main);
-            }
-        });
+        if (adapter.supportsFeature && adapter.supportsFeature('PLUGINS')) {
+            loadExistingAccessories(main);
+        }
+        else {
+            initSentry(() => loadExistingAccessories(main));
+        }
     });
     
     return adapter;
@@ -2569,11 +2634,16 @@ function main() {
         adapter.log.info('Alexa-Push-Connection Unknown Command ' + command + ' - send to Developer: ' + JSON.stringify(payload));
     });
 
+    let listsInProgress = {};
     alexa.on('ws-todo-change', (payload) => {
         adapter.log.debug('Received updated list: ' + JSON.stringify(payload));
-		
+
+        if (listsInProgress[payload.listId]) return;
+
+        listsInProgress[payload.listId] = true;
 		alexa.getList(payload.listId, (err, list) => {
             if (!list) return;
+            delete listsInProgress[payload.listId];
             // modify states
 			list.name = list.name || list.type;
 			if (!list.name) return;
@@ -2590,11 +2660,13 @@ function main() {
 				
 				// delete objects
 				let node = adapter.namespace + '.Lists.' + list.id + '.items.' + payload.listItemId;
+                listsInProgress[payload.listId] = true;
 				adapter.getObjectList({startkey: node, endkey: node + '.\u9999'}, (err, objects) => {
-					
-					if (objects && objects.rows) {
+
+                    if (objects && objects.rows) {
 						objects.rows.forEach(object => adapter.delObject(object.id));
 					}
+                    delete listsInProgress[payload.listId];
 				});
 			}
 		});
